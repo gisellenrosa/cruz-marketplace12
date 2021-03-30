c43483b58583b4492b2f7a6b6725e9dfbf2e6d66
Igor Meira do Nascimento Silva <igormnsilva@gmail.com>
2021-03-30 18:25:51 -0300

    imagem não altera de tamanho. Primeiro PR

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,7 +8,7 @@ const MainDiv = styled.div`
   grid-template-row; 10px 1fr 10px;
   justify-items: center;
   align-items: center;
-  
+  height: 100vh;
 `;
 
 const ContentImg = styled.img`
@@ -22,6 +22,9 @@ const ContentImg = styled.img`
 const Intro = styled.h3`
 margin: 0;
 background-color: #E8ECEF;
+height: 60px;
+width: 600px;
+text-align: center;
 `
 
 export default class HomePage extends React.Component {
@@ -31,12 +34,12 @@ export default class HomePage extends React.Component {
         <ContentImg
           src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F6af9eda3-f4a0-46a8-96da-d290e1102a19%2Ffuturecar.png?table=block&id=56447354-1825-4378-b040-156dd065d298&width=3100&userId=4c407320-0851-42db-9ba2-49db837591f3&cache=v2"
           style={{ width: "700px" }}
-        />
+          />
+          <Intro>
+            A FutureCar e uma empresa que busca conectar vendedores e compradores
+            de carros de forma simples e eficaz
+          </Intro>
 
-        <Intro>
-          A FutureCar e uma empresa que busca conectar vendedores e compradores
-          de carros de forma simples e eficaz
-        </Intro>
       </MainDiv>
     );
   }
