a78370ac141201646848b02dfa98fa5d6ff9d875
William Gomes <76756233+WilliamGofe@users.noreply.github.com>
2021-03-31 16:04:47 -0300

    Pagina de venda estilizada
    Página de venda estilizada o mais parecido possivel do figma

--- src/pages/SellPage.js
+++ src/pages/SellPage.js
@@ -1,5 +1,77 @@
 import axios from "axios";
 import React from "react";
+import styled from  "styled-components";
+
+const FlexColumn = styled.div`
+display:flex;
+flex-direction:column;
+`
+const InicioSellPage = styled.div`
+font-size:24px;
+color: #4d6f80;
+`
+const HrSellPage = styled.hr `
+color: #4d6f80;
+height:2px;
+background-color:#4d6f80;
+opacity:0.3;
+`
+const DivMasterSellPage = styled.div`
+width:50%;
+margin:auto;
+`
+const InputSellPage = styled.input`
+border-bottom:solid;
+border-width:1px;
+height:40px;
+border-radius:12px;
+border-color:#d3e2e5;
+margin-bottom:16px;
+` 
+const InputSellPage2 = styled.input`
+border-bottom:solid;
+border-width:1px;
+height:120px;
+border-radius:12px;
+border-color:#d3e2e5;
+margin-bottom:16px;
+` 
+const LabelSellpage = styled.label`
+margin-bottom:12px;
+font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+font-weight:bold;
+color:#545454;
+font-size:20px;
+`
+const MetodoPrecoPrazo = styled.div `
+display:flex;
+font-weight:bold;
+font-size:16px;
+justify-content:space-around;
+`
+const SelectSellPag = styled.select`
+border-bottom:solid;
+border-width:1px;
+height:50px;
+border-radius:12px;
+border-color:#8888;
+margin-bottom:16px;
+border-right:none;
+border-bottom:none;
+`
+
+const ButtonSellPage = styled.button`
+background-color:#ff7d7d;
+border-radius:25px;
+border-color:transparent;
+width:100%;
+margin-top:16px;
+height:48px;
+font-weight:bold;
+font-size:20px;
+color:white;
+`
+
 
 export default class SellPage extends React.Component {
   state={
@@ -56,42 +128,83 @@ export default class SellPage extends React.Component {
   render() {
   
     return (
-      <div>
+      <DivMasterSellPage>
+        <InicioSellPage>
+        <h2>Anuncie seu Carro</h2>
+        <HrSellPage/>
+        </InicioSellPage>
+       
+        <FlexColumn>
+          <LabelSellpage>
+            Nome
+          </LabelSellpage>
+          
 
-        <h1>EM CONSTRUÇÃO</h1>
-        <label>Nome</label>
-        <input
-         onChange={this.handleName}
-         value={this.state.name}
-         />
-         <label>Descrição</label>
-         <input
-         onChange={this.handleDescription}
-         value={this.state.description}
-         />
-         <label>Metodo de Pagamento</label>
-         <select onChange={this.handlePaymentMethod} value={this.state.paymentMethod}>
-          <option value="Dinheiro">Dinheiro</option>
+          <InputSellPage onChange={this.handleName}
+         value={this.state.name} />
+          
+          <LabelSellpage>
+            Descrição
+          </LabelSellpage>
+         
+       
+        <InputSellPage2 onChange={this.handleDescription}
+         value={this.state.description}/>
+
+         </FlexColumn>
+         
+        
+        <MetodoPrecoPrazo>
+          <FlexColumn>
+            
+            <LabelSellpage>
+              Metodo de Pagamento
+            </LabelSellpage>
+
+          <SelectSellPag onChange={this.handlePaymentMethod} value={this.state.paymentMethod}>
+             <option value="Dinheiro">Dinheiro</option>
           <option value="Cartão de Crédito">Cartão de Crédito</option>
           <option value="Cartão de Débito">Cartão de Débito</option>
           <option value="Boleto Bancário">Boleto Bancário</option>
           <option value="Pix">Pix</option>
-          </select>
-         <label>Preço</label>
-         <input type="number"
+          </SelectSellPag>
+         
+          </FlexColumn>
+         
+
+        <FlexColumn>
+          <LabelSellpage>
+            Preço
+          </LabelSellpage>
+          
+           <InputSellPage type="number"
          onChange={this.handlePrice}
-         value={this.state.price}
-         />
-          <label>Tempo de Entrega</label>
-         <input type="number"
+         value={this.state.price} />
+
+        </FlexColumn>
+        
+        <FlexColumn>
+          <LabelSellpage>
+            Tempo de Entrega
+          </LabelSellpage>
+           
+           <InputSellPage type="number"
          onChange={this.handleShipping}
-         value={this.state.shipping}
-         />
+         value={this.state.shipping}/>
+           
+         
+       
+        </FlexColumn>
+         
 
-         <button onClick={this.createCar}>Anunciar Carro</button>
-        <h2></h2>
-
-      </div>
+        </MetodoPrecoPrazo>
+        
+        <ButtonSellPage onClick={this.createCar}>
+        Anunciar
+        </ButtonSellPage>
+       
+        
+         </DivMasterSellPage>
     );
   }
 }
