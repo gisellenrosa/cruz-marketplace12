bf5cfafa288131ffd7df57a679aa398e243a38f5
Igor Meira do Nascimento Silva <igormnsilva@gmail.com>
2021-03-31 10:52:54 -0300

    design homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,12 +15,12 @@ const ContentImg = styled.img`
   align-items: center;
   display: flex;
   justify-content: center;
-  margin: -50px;
-  padding: -50px;
+  margin-top: -350px;
 `;
 
 const Intro = styled.h3`
-margin: -150px;
+display: flex;
+margin-top: -725px;
 background-color: #E8ECEF;
 height: 60px;
 width: 600px;
@@ -33,7 +33,7 @@ export default class HomePage extends React.Component {
       <MainDiv>
         <ContentImg
           src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F6af9eda3-f4a0-46a8-96da-d290e1102a19%2Ffuturecar.png?table=block&id=56447354-1825-4378-b040-156dd065d298&width=3100&userId=4c407320-0851-42db-9ba2-49db837591f3&cache=v2"
-          style={{ width: "500px" }}>
+          style={{ width: "620px" }}>
           </ContentImg>
           
 
